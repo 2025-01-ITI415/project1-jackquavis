93f7e36ae1ded85d49f7e18e74c3d1a2ceb52551
jackquavis <125089721+jackquavis@users.noreply.github.com>
2025-02-27 20:01:29 -0500

    Many Additions
    Fixed Wall Jumping, added platforms, added walls

--- Assets/__Scripts/PlayerController.cs
+++ Assets/__Scripts/PlayerController.cs
@@ -6,8 +6,15 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
 
-    public float maxX = 8.4f;
+    //public float maxX = 8.4f;
     public float speed;
+    public float jumpPower;
+    private bool canJump = false;
+    private bool canRWallJump = false;
+    private bool canLWallJump = false;
+    private bool canWallJump = false;
+    private string tempWallCheck;
+
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -15,22 +22,88 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate ()
-	{
-		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
+    void FixedUpdate () {
+		// Movement Left and Right
 		float moveHorizontal = Input.GetAxis ("Horizontal");
+		Vector3 lrMovement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+        rb.AddForce (lrMovement * speed, ForceMode.Impulse);
+        //rb.MovePosition(rb.position + lrMovement * speed * Time.deltaTime);
 
-		// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+        //Jumping (if statements to check if player is on ground)
+        float jump = Input.GetAxis("Jump");
+        Vector3 jumpMovement = new Vector3 (0.0f, jump, 0.0f);
+        Vector3 jumpRMovement = new Vector3 (jump, jump, 0.0f);
+        Vector3 jumpLMovement = new Vector3 (-jump, jump, 0.0f);
 
-		// Add a physical force to our Player rigidbody using our 'movement' Vector3 above, 
-		// multiplying it by 'speed' - our public player speed that appears in the inspector
-		rb.AddForce (movement * speed);
+        /*
+        if ((canJump && canWallJump) == true) {
+            canWallJump = false;
+            if (canRWallJump == true) {
+                canRWallJump = false;
+                tempWallCheck = "right";
+            }
+            if (canLWallJump == true) {
+                canLWallJump = false;
+                tempWallCheck = "left";
+            }
+        }
+        */
+
+        if (canJump == true) {
+            //rb.AddForce (jumpMovement * jumpPower); 
+            rb.AddForce (jumpMovement * jumpPower, ForceMode.Impulse);
+            //rb.MovePosition(rb.position-1 * speed * Time.deltaTime);
+        }
+        if (canRWallJump == true && canWallJump == true) {
+            //rb.AddForce (jumpRMovement * jumpPower);
+            rb.AddForce (jumpRMovement * jumpPower, ForceMode.Impulse);
+        }
+        if (canLWallJump == true && canWallJump == true) {
+            //rb.AddForce (jumpLMovement * jumpPower);
+            rb.AddForce (jumpLMovement * jumpPower, ForceMode.Impulse);
+        }
 	}
 
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.CompareTag("Ground")) {
+            canJump = true;
+            canWallJump = true;
+        }
+        if (col.gameObject.CompareTag("RWall")) {
+            canRWallJump = true;
+        }
+        if (col.gameObject.CompareTag("LWall")) {
+            canLWallJump = true;
+        }
+    }
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.CompareTag("Ground")) {
+            canJump = false;
+            
+            /*
+            if ((col.gameObject.CompareTag("LWall") || col.gameObject.CompareTag("RWall")) == false) {
+                canWallJump = true;
+                if (tempWallCheck == "right") {
+                    canRWallJump = true;
+                } else if (tempWallCheck == "left") {
+                    canLWallJump = true;
+                }
+            }*/
+        }
+        if (col.gameObject.CompareTag("RWall")) {
+            canRWallJump = false;
+            canWallJump = false;
+        }
+        if (col.gameObject.CompareTag("LWall")) {
+            canLWallJump = false;
+            canWallJump = false;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        if 
+        
     }
 }
